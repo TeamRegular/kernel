5645f981e38cdd8a1ef1eeaa0b6613a0792b79fe
Alexandre Courbot <acourbot@nvidia.com>
2013-06-05 16:10:25 +0900

    [HACK] roth: init display clocks
    Necessary to keep the display on after boot
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- drivers/clk/tegra/clk-tegra114.c
+++ drivers/clk/tegra/clk-tegra114.c
@@ -2173,6 +2173,9 @@ static __initdata struct tegra_clk_init_table init_table[] = {
 	{i2s4, pll_a_out0, 11289600, 0},
 	{dfll_soc, pll_p, 51000000, 1},
 	{dfll_ref, pll_p, 51000000, 1},
+	{disp1, clk_max, 0, 1},
+	{dsia, clk_max, 0, 1},
+	{blink, clk_max, 0, 1},
 	{clk_max, clk_max, 0, 0}, /* This MUST be the last entry. */
 };
 
